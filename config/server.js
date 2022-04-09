c0d2aa8cb8335a1750ff6952d031c753f198ecdf
cpinopacheco <cpinopacheco64@gmail.com>
2022-04-08 21:44:51 -0400

    Deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'efd56a745ac578d3858ab90c40abacca'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a3ef1962-7a64-4a8f-86c6-61d0ea404969'
+};
